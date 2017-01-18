a5fe0a8b5d19c8c77e106e12b41bc2d4f63e4b1f
yo-no <spencerblake91@gmail.com>
2017-01-17 21:27:42 -0600

    changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+5.times do 
+  product = Product.new(name: Faker::Name.name+ " T-Shirt", size:"L", color: "White", price: rand(1000), description: Faker::Hipster.paragraph(2), image:"http://pngimg.com/upload/tshirt_PNG5426.png")
+  product.save
+end
